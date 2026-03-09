844cb5087359a825bea63d96927042b0548d1d32
José Estevão <estevaotujimoto@gmail.com>
2026-03-09 14:05:29 -0300

    Adicionando exemplos de funcoes em Python

---
+++ funcoes_python.py
@@ -0,0 +1,47 @@
+# def saudacao():
+#     print("Olá, seja bem-vindo!")
+
+# saudacao()
+
+
+
+
+# def soma():
+#     num1 = 5
+#     num2 = 3
+#     print(num1 + num2)
+
+# soma()
+
+
+
+# def soma(a, b):
+#     print(a + b)
+
+# soma(10, 5)
+
+
+
+# def soma(a, b):
+#     return a + b
+
+# resultado = soma(4, 6)
+
+# print(resultado)
+
+
+
+# def verificar_idade(idade):
+
+#     if idade < 18:
+#         return "Menor de idade"
+#     else:
+#         return "Maior de idade"
+
+# print(verificar_idade(20))
+
+
+# def dobro(numero):
+#     return numero * 2
+
+# print(dobro(5))
